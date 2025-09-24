4148857ee8ac38c2cb98be84cfe9f6b881815e0d
Andrew Purchase <andrewpurchase@device.local>
2025-09-24 09:31:39 -0700

    adding pyspark functionality

---
+++ data/data_definitions/warehouse.sql
@@ -0,0 +1,14 @@
+-- create table my_table (
+--     name varchar(255),
+--     location varchar(50)
+-- )
+-- using parquet
+-- location '/Users/andrewpurchase/Documents/electicity-demand/warehouse';
+
+-- create table my_table2 (
+--     name varchar(255),
+--     location varchar(50)
+-- )
+-- using parquet
+-- location '/Users/andrewpurchase/Documents/electicity-demand/warehouse';
+drop table my_table2
