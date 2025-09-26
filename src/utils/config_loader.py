ce4f7358c9edef0f219c533a610a99530887eeac
Andrew Purchase <andrewpurchase@device.local>
2025-09-26 14:51:44 -0700

    updating gitignore

---
+++ src/utils/config_loader.py
@@ -0,0 +1,32 @@
+<<<<<<< HEAD
+=======
+from dotenv import load_dotenv
+from pyspark.sql import SparkSession
+from src.utils import shared_helpers as sh
+import configparser
+import pyspark
+from delta import *
+import os
+
+# Load environment variables from the .env file
+load_dotenv()
+
+# Retrieve environment variable to determine config file
+env = os.getenv("APP_ENV")
+config_file = f"config/config.{env}.ini"
+
+# Load configuration from the specified INI file
+config = configparser.ConfigParser()
+config.read(config_file)
+
+# Initialize Spark session with the specified warehouse directory
+database_root_path = config.get("DEFAULT", "database_root_path")
+
+builder = (
+    pyspark.sql.SparkSession.builder.appName("data_pipelines")
+    .config("spark.sql.warehouse.dir", database_root_path)
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+)
+spark = configure_spark_with_delta_pip(builder).getOrCreate()
+>>>>>>> ff2e0e3 (updating gitignore)
