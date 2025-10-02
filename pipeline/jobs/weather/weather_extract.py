5c8a8dad956619aef73878fbe977dc682689bab5
Andrew Purchase <andpurch@gmail.com>
2025-10-02 16:23:59 -0700

    POC Weather Extraction

--- pipeline/jobs/weather/weather_extract.py
+++ pipeline/jobs/weather/weather_extract.py
@@ -1,22 +1,7 @@
-from pyspark.sql import SparkSession
-from pipeline.utils.shared_helpers import read_api
-from dotenv import load_dotenv
-import os
 from pipeline.utils.config_loader import get_env_variable, load_config_file, spark_session
 
-
-# def extract_weather_data():
-#     spark = SparkSession.builder.getOrCreate()
-#     api_url = "https://api.weather.com/data"
-#     df = read_api(api_url)  # Shared function to read API data
-#     save_as_table(df, "raw_weather")  # Shared function to save as a table
-#     return df
-
-
-# if __name__ == "__main__":
-#     extract_weather_data()
-
-
+from dotenv import load_dotenv
+import requests, os
 import pyspark
 from delta import *
 
@@ -34,6 +19,82 @@ env = get_env_variable()
 config_params = load_config_file(env)
 database_path_root = config_params.get("database_path_root", "./tmp/delta")
 
-load_dotenv()
 
-weather_api_key = os.getenv("WEATHER_API_KEY")
+load_dotenv()
+api_url = "http://api.weatherapi.com/v1/history.json"
+api_key = os.getenv("WEATHER_API_KEY")
+
+params = {"key": api_key, "q": "Portland", "dt": "2025-09-27", "end_dt": "2025-10-01"}
+response = requests.get(api_url, params=params)
+if response.status_code == 200:
+    df = response.json()
+else:
+    print(f"Error: {response.status_code}")
+
+rows = []
+for day in df["forecast"]["forecastday"]:
+    for hour in day["hour"]:
+        rows.append(
+            {
+                "date": day["date"],
+                "time": hour["time"],
+                "temp_c": hour["temp_c"],
+                "temp_f": hour["temp_f"],
+                "condition": hour["condition"]["text"],
+                "is_day": hour["is_day"],
+                "precip_mm": hour["precip_mm"],
+                "snow_cm": hour["snow_cm"],
+                "humidity": hour["humidity"],
+                "cloud": hour["cloud"],
+                "feelslike_c": hour["feelslike_c"],
+                "windchill_c": hour["windchill_c"],
+                "heatindex_c": hour["heatindex_c"],
+                "dewpoint_c": hour["dewpoint_c"],
+                "will_it_rain": hour["will_it_rain"],
+                "chance_of_rain": hour["chance_of_rain"],
+                "will_it_snow": hour["will_it_snow"],
+                "chance_of_snow": hour["chance_of_snow"],
+                "wind_dir": hour["wind_dir"],
+                "pressure_mb": hour["pressure_mb"],
+                "wind_kph": hour["wind_kph"],
+                "vis_km": hour["vis_km"],
+                "gust_kph": hour["gust_kph"],
+                "uv": hour["uv"],
+                "sunrise": day["astro"]["sunrise"],
+                "sunset": day["astro"]["sunset"],
+            }
+        )
+
+df = spark.createDataFrame(
+    data=rows,
+    schema=[
+        "sunrise",
+        "sunset",
+        "date",
+        "time",
+        "temp_c",
+        "temp_f",
+        "condition",
+        "is_day",
+        "precip_mm",
+        "snow_cm",
+        "humidity",
+        "cloud",
+        "feelslike_c",
+        "windchill_c",
+        "heatindex_c",
+        "dewpoint_c",
+        "will_it_rain",
+        "chance_of_rain",
+        "will_it_snow",
+        "chance_of_snow",
+        "wind_dir",
+        "pressure_mb",
+        "wind_kph",
+        "vis_km",
+        "gust_kph",
+        "uv",
+    ],
+)
+df.printSchema()
+df.write.format("delta").mode("append").save(f"{database_path_root}/weather_extract")
